d5bb8fa7077b04a1b85a2af56bc5323555f0b729
ZBM Zubayer <zbmzubayer@gmail.com>
2023-03-07 23:15:13 +0600

    TypeOrm forFreature added for all entities

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,4 +1,35 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Admin } from './entity/admin.entity';
+import { Category } from './entity/category.entity';
+import { Customer } from './entity/customer.entity';
+import { CustomerPayment } from './entity/customerPayment.entity';
+import { Employee } from './entity/employee.entity';
+import { Order } from './entity/order.entity';
+import { OrderDetail } from './entity/orderDetail.entity';
+import { Payment } from './entity/payment.entity';
+import { Product } from './entity/product.entity';
+import { ProductPhoto } from './entity/productPhoto.entity';
+import { Review } from './entity/review.entity';
+import { Seller } from './entity/seller.entity';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Admin,
+      Employee,
+      Seller,
+      Customer,
+      Product,
+      ProductPhoto,
+      Category,
+      Order,
+      OrderDetail,
+      Payment,
+      CustomerPayment,
+      Review,
+    ]),
+  ],
+  exports: [TypeOrmModule],
+})
 export class DbModule {}
