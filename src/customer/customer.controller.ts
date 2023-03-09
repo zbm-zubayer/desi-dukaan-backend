73e9859ca2571e1b99585dbed6872c0887640b18
ZBM Zubayer <zbmzubayer@gmail.com>
2023-03-10 00:29:06 +0600

    Customer crud, pass hashing and file upload added

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,4 +1,88 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  Put,
+  Query,
+  UploadedFile,
+  UseInterceptors,
+  ValidationPipe,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { saveUploadedFile } from 'src/helper/saveUploadedFile';
+import { CustomerService } from './customer.service';
+import { CustomerChangePassDto } from './dto/cChangePass.dto';
+import { CustomerUpdateDto } from './dto/cProfileUpdate.dto';
+import { CustomerRegisterDto } from './dto/cRegister.dto';
 
 @Controller('customer')
-export class CustomerController {}
+export class CustomerController {
+  constructor(private customerService: CustomerService) {}
+
+  @Post('/register')
+  registerAccount(@Body(ValidationPipe) customerRegisterDto: CustomerRegisterDto) {
+    return this.customerService.registerAccount(customerRegisterDto);
+  }
+  @Get('/profile/:id')
+  viewProfile(@Param('id', ParseUUIDPipe) uuid) {
+    return this.customerService.viewProfile(uuid);
+  }
+  @Put('/profile/:id')
+  @UseInterceptors(FileInterceptor('C_Photo', saveUploadedFile))
+  updateProfile(
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe) customerUpdateDto: CustomerUpdateDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    if (!file) {
+      customerUpdateDto.C_Photo = null;
+      return this.customerService.updateProfile(id, customerUpdateDto);
+    } else {
+      console.log(file);
+      customerUpdateDto.C_Photo = file.filename;
+      return this.customerService.updateProfile(id, customerUpdateDto);
+    }
+  }
+  @Delete('/delete/:id')
+  deleteAccount(@Param('id', ParseIntPipe) id: number) {
+    return this.customerService.deleteAccount(id);
+  }
+  @Patch('/change-password/:id')
+  changePassword(@Param('id', ParseIntPipe) id: number, @Body(ValidationPipe) customerChangePassDto: CustomerChangePassDto) {
+    return this.customerService.changePassword(id, customerChangePassDto);
+  }
+  @Post('/forgot-password')
+  forgotPassword() {
+    return this.customerService.forgotPassword();
+  }
+  @Get('/product')
+  viewAllProduct() {
+    return this.customerService.viewAllProduct();
+  }
+  @Get('/product/:id')
+  viewProductDetail(@Param('id', ParseIntPipe) id: number) {
+    return this.customerService.viewProductDetail(id);
+  }
+  @Get('/s')
+  searchProduct(@Query() query) {
+    return this.customerService.searchProduct(query);
+  }
+  @Get('/cart')
+  viewCart() {}
+  @Get('/order')
+  viewAllOrder() {
+    return this.customerService.viewAllOrder();
+  }
+  @Get('/order/:id')
+  viewOrderDetail() {
+    return this.customerService.viewOrderDetail();
+  }
+  @Get('/payment')
+  viewPayment() {}
+}
--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,4 +1,81 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+import { Customer } from 'src/db/entity/customer.entity';
+import { Order } from 'src/db/entity/order.entity';
+import { Payment } from 'src/db/entity/payment.entity';
+import { Product } from 'src/db/entity/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class CustomerService {}
+export class CustomerService {
+  constructor(
+    @InjectRepository(Customer) private customerRepo: Repository<Customer>,
+    @InjectRepository(Product) private productRepo: Repository<Product>,
+    @InjectRepository(Order) private orderRepo: Repository<Order>,
+    @InjectRepository(Payment) private paymentRepo: Repository<Payment>,
+  ) {}
+  // Customer Register
+  async registerAccount(customerRegisterDto) {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(customerRegisterDto.C_Password, salt);
+    customerRegisterDto.C_Password = hashedPassword;
+    customerRegisterDto.C_CreatedAt = new Date();
+    customerRegisterDto.C_ModifiedAt = new Date();
+    return await this.customerRepo.save(customerRegisterDto);
+  }
+  // Customer Profile
+  async viewProfile(uuid) {
+    return await this.customerRepo.findOneBy({ C_Uuid: uuid });
+  }
+  // Customer Update Profile
+  async updateProfile(id, customerUpdateDto) {
+    customerUpdateDto.C_ModifiedAt = new Date();
+    return await this.customerRepo.update(id, customerUpdateDto);
+  }
+  // Customer Update Profile Photo
+  async uploadProfilePhoto(id, customerUpdateDto) {
+    return await this.customerRepo.update(id, customerUpdateDto);
+  }
+  // Customer Delete Account
+  async deleteAccount(id) {
+    return await this.customerRepo.delete(id);
+  }
+  // Customer Change Password
+  async changePassword(id, customerChangePassDto) {
+    const dbPassword = await (await this.customerRepo.findOneBy({ C_Id: id })).C_Password;
+    const isMatch = await bcrypt.compare(customerChangePassDto.C_CurrentPassword, dbPassword);
+    if (isMatch) {
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(customerChangePassDto.C_NewPassword, salt);
+      customerChangePassDto.C_NewPassword = hashedPassword;
+      return await this.customerRepo.update({ C_Id: id }, { C_Password: customerChangePassDto.C_NewPassword });
+    } else {
+      return 'Password not match';
+    }
+  }
+  // Customer Forgot Password
+  async forgotPassword() {}
+
+  viewAllProduct() {
+    return this.productRepo.find();
+  }
+  // Customer View Product Detail
+  viewProductDetail(id) {
+    return this.productRepo.findOneBy({ P_Id: id });
+  }
+  // Customer Search Product
+  searchProduct(name) {
+    return this.productRepo.findOneBy({ P_Name: name.q });
+  }
+  // Customer View Cart
+  async viewCart() {}
+  // Customer View All Order
+  async viewAllOrder() {
+    return await this.orderRepo.find();
+  }
+  // Customer View Order Detail
+  async viewOrderDetail() {}
+  // Customer View Payment
+  async viewPayment() {}
+}
